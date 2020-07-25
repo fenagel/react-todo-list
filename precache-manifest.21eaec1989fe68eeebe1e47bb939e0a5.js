715639ac818094fbedbf61c3a4446b295ebcceb1
Felix Nagel <felixnagel105@gmail.com>
2020-07-25 12:58:15 +0200

    Updates

---
+++ precache-manifest.21eaec1989fe68eeebe1e47bb939e0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffb37dec20fc0a218f514fd4ecb1f62",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "9b5010c08171996e3b50",
+    "url": "/react-todo-list/static/css/main.f03938c8.chunk.css"
+  },
+  {
+    "revision": "9ffe569b406a0be0de74",
+    "url": "/react-todo-list/static/js/2.19e70c71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.19e70c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5010c08171996e3b50",
+    "url": "/react-todo-list/static/js/main.20f2cec5.chunk.js"
+  },
+  {
+    "revision": "24763f4ac37c4c84a6e5",
+    "url": "/react-todo-list/static/js/runtime-main.b105f84a.js"
+  }
+]);
